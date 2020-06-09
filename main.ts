353edf261cc2196d666042f8fe74b7f241e3ef03
sjanulonoks <sjanulonoks@users.noreply.github.com>
2020-06-09 22:15:05 +0200

    m

--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
  namespace ps2controller {
 
-    //% block="ps2 to spi mapping: mosi$ps2-cmd_spi-mosi miso%ps2-data_spi-miso sck%ps2-clock_spi-sck cs%ps2-att_spi-ss"
-    //% ps2-cmd_spi-mosi.defl=DigitalPin.P15
-    //% ps2-data_spi-miso.defl=DigitalPin.P14
-    //% ps2-clock_spi-sck.defl=DigitalPin.P13
-    //% ps2-att_spi-ss.defl=DigitalPin.P12
+    //% block="ps2 to spi mapping: mosi$ps2cmd_spimosi miso%ps2data_spimiso sck%ps2clock_spisck cs%ps2att_spiss"
+    //% ps2cmd_spimosi.defl=DigitalPin.P15
+    //% ps2data_spimiso.defl=DigitalPin.P14
+    //% ps2clock_spisck.defl=DigitalPin.P13
+    //% ps2att_spiss.defl=DigitalPin.P12
 
-    export function SPI_init(ps2-att_spi-ss:DigitalPin,ps2-cmd_spi-mosi:DigitalPin,ps2-data_spi-miso:DigitalPin,ps2-clock_spi-sck:DigitalPin) {
+    export function SPI_init(ps2att_spiss:DigitalPin,ps2cmd_spimosi:DigitalPin,ps2data_spimiso:DigitalPin,ps2clock_spisck:DigitalPin) {
         //ps2controller.SPI_init(DigitalPin.P12,DigitalPin.P8, DigitalPin.P14, DigitalPin.P13)
             // http://blog.nearfuturelaboratory.com/2008/06/19/playstation2-logic-analysis/
             // https://makecode.microbit.org/reference/pins/spi-pins
             // https://forum.makecode.com/t/ps2-controller-extension/1409
             // http://www.techmonkeybusiness.com/using-a-playstation-2-controller-with-your-arduino-project.html
 
-            chipSelect = ps2-att_spi-ss;
+            chipSelect = ps2att_spiss;
             pins.digitalWritePin(chipSelect, 1)
-            pins.spiPins(ps2-cmd_spi-mosi, ps2-data_spi-miso, ps2-clock_spi-sck);
+            pins.spiPins(ps2cmd_spimosi, ps2data_spimiso, ps2clock_spisck);
             pins.spiFormat(8, 3);
             pins.spiFrequency(250000);
        }
